3d3f573af03b86f8e44b18479177db6a8e487e8a
Konstantin Prokofiev <flashboyz@yandex.ru>
2021-07-06 18:34:09 +0300

    setup style for elements

--- trafficLightApp/ViewController.swift
+++ trafficLightApp/ViewController.swift
@@ -8,12 +8,27 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    @IBOutlet weak var redLightView: UIView!
+    @IBOutlet weak var yellowLightView: UIView!
+    @IBOutlet weak var greenLightView: UIView!
+    @IBOutlet weak var startButton: UIButton!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        redLightView.layer.cornerRadius = 75
+        redLightView.alpha = 0.4
+        yellowLightView.layer.cornerRadius = 75
+        yellowLightView.alpha = 0.4
+        greenLightView.layer.cornerRadius = 75
+        greenLightView.alpha = 0.4
+        startButton.layer.cornerRadius = 10
+        
     }
-
-
+    @IBAction func startButtonPressed() {
+        
+    }
+    
+    
 }
 
